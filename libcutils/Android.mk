a9e497036862976238425369123f1b528e6d2880
xplodwild <xplod@ouverta.fr>
2013-10-08 18:14:47 +0200

    Add pmem dev support in libcutils
    Change-Id: I32a0630238444389bb6a0c1a4ff07c1b08a2834a

--- libcutils/Android.mk
+++ libcutils/Android.mk
@@ -116,7 +116,31 @@ LOCAL_SRC_FILES := $(commonSources) \
         properties.c \
         qtaguid.c \
         trace.c \
-        uevent.c \
+        uevent.c
+
+ifeq ($(TARGET_BOARD_PLATFORM),mt6589)
+LOCAL_SRC_FILES += pmem-dev.cpp
+endif # $(TARGET_BOARD_PLATFORM),mt6589
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SRC_FILES += arch-arm/memset32.S
+else  # !arm
+ifeq ($(TARGET_ARCH_VARIANT),x86-atom)
+LOCAL_CFLAGS += -DHAVE_MEMSET16 -DHAVE_MEMSET32
+LOCAL_SRC_FILES += arch-x86/android_memset16.S arch-x86/android_memset32.S memory.c
+else # !x86-atom
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_SRC_FILES += arch-mips/android_memset.c
+else # !mips
+LOCAL_SRC_FILES += memory.c
+endif # !mips
+endif # !x86-atom
+endif # !arm
+
+ifneq ($(TARGET_RECOVERY_PRE_COMMAND),)
+    LOCAL_CFLAGS += -DRECOVERY_PRE_COMMAND='$(TARGET_RECOVERY_PRE_COMMAND)'
+endif
+
 
 LOCAL_SRC_FILES_arm += \
         arch-arm/memset32.S \
